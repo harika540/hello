07f0ee5c5f158fc10adce0284ee93117f006f3bb
harika540 <41327343+harika540@users.noreply.github.com>
2018-07-19 15:24:07 +0530

    num1

---
+++ num1.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class num1
+{
+   public static void main(String arg[])
+   {
+      File f=new File("hari.txt");
+      boolean CreateNewFile=f.CreateNewFile();
+      System.out.println("success");
+    }
+}
